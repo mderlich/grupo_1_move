bc5464f01bf01c7aac06c34fcb2ee9b61b53e763
estefaniapottassa <estefania.pottassa@gmail.com>
2022-06-06 15:10:53 -0300

    se crearon los archivos html

--- app.js
+++ app.js
@@ -9,4 +9,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productCart.html'))
+})
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
 app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
