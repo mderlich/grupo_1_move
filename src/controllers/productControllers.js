12121a7785e9b81a575fc90cef57a9ef9cd19d89
Matias Derlich <mderlich@gmail.com>
2022-06-16 16:08:28 -0300

    nuevas carpetas y archivos segun MVC

---
+++ src/controllers/productControllers.js
@@ -0,0 +1,13 @@
+/* 
+let carritoController = {
+   
+    sacarItem: function(){},
+    consultarItem: function(){},
+    agregarItem: function (req, res) {
+       res.send('Item Agregado');
+    },
+    
+ };
+ 
+ module.exports = carritoController
+  */
