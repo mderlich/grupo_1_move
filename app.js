d2683a3cef0db3f2112f28dab509a3843bbf6046
estefaniapottassa <estefania.pottassa@gmail.com>
2022-06-25 15:34:59 -0300

    se implementaron los ruteos

--- app.js
+++ app.js
@@ -3,20 +3,10 @@ const path = require('path');
 const app = express();
 app.use(express.static('public'));
 
-// ---------------------------------------------------------------
-// ruta hacia... home
-app.get('/', (req, res) => { res.sendFile( path.join(__dirname, '/views/home.html') ); });
-app.post('/', (req, res) => { res.sendFile( path.join(__dirname, '/views/home.html') ); });
+app.set('view engine', 'ejs');
 
-// ruta hacia... login
-app.get('/login', (req, res) => { res.sendFile( path.join(__dirname, '/views/login.html') ); });
-// ruta hacia... productCart
-app.get('/productCart', (req, res) => { res.sendFile( path.join(__dirname, '/views/productCart.html') ); });
-// ruta hacia... productDetail
-app.get('/productDetail', (req, res) => { res.sendFile( path.join(__dirname, '/views/productDetail.html') ); });
-// ruta hacia... register
-app.get('/register', (req, res) => { res.sendFile( path.join(__dirname, '/views/register.html') ); });
-// ---------------------------------------------------------------
+const mainRoutes = require('./src/routes/main');
+app.use('/', mainRoutes);
 
 // el puerto segun indiquen
 // Modificacion para emplear en Heroku... 'process.env.PORT ||'
---
+++ src/routes/main.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get('/', (req, res) => { 
+    res.render('home') });
+    
+router.post('/', (req, res) => { 
+    res.render('home') });
+
+// ruta hacia... login
+router.get('/login', (req, res) => { 
+    res.render( 'login') });
+
+// ruta hacia... productCart
+router.get('/productCart', (req, res) => { 
+    res.render('productCart') });
+
+// ruta hacia... productDetail
+router.get('/productDetail', (req, res) => { 
+    res.render( 'productDetail') });
+
+// ruta hacia... register
+router.get('/register', (req, res) => { 
+    res.render( 'register') });
+
+// ruta hacia... forget password
+router.get('/forgetpw', (req, res) => { 
+    res.render( 'forgetpw') });
+
+module.exports = router;
