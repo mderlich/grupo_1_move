d278b8f1f21679b4750002ef267d1cf6c2cda53b
Matias Derlich <mderlich@gmail.com>
2022-06-27 16:01:34 -0300

    inclui informacion estructurada de referencia para emplear

---
+++ src/database/ventasData.js
@@ -0,0 +1,32 @@
+/* 
+
+Convertidor de EXCEL a JSON
+https://products.aspose.app/cells/es/conversion/excel-to-json 
+
+1) se armo tabla en excel
+2) se paso a json en convertidor en linea
+3) se limpio y acomodo un poco aqui en VSCODE
+
+*/
+
+const ventas = [
+
+    {
+        id: 1,
+        item: "PU_374915",
+        talle: 36,
+        fecha: "06/27/2022",
+        precio: 14999
+    },
+
+    {
+        id: 2,
+        item: "PU_388265",
+        talle: 37,
+        fecha: "06/20/2022",
+        precio: 19999
+    }
+
+];
+
+module.exports = ventas;
