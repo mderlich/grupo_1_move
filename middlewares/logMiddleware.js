91caf2c84f1a4854e0337d1f634ab1f1126b01cb
Rodrione1 <alvarezrodrigofernando@gmail.com>
2022-07-11 21:48:06 -0300

    logMiddleware resolviendo archivo txt

--- middlewares/logMiddleware.js
+++ middlewares/logMiddleware.js
@@ -1,6 +1,6 @@
 const fs = require ('fs');
 function logMiddleware (req,res,next){
-fs.appendFileSync('log.txt','Se ingreso a la página' +req.url)
+fs.appendFileSync('log.txt','Se ingreso a la página' +req.url + "\n")
 next();
 }
 module.exports = logMiddleware;
