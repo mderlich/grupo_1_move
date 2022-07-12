3278847dc8de158e7a8fd00ee89af2612820eff0
Rodrione1 <alvarezrodrigofernando@gmail.com>
2022-07-11 22:42:57 -0300

    logMiddleware,logDBMiddleware.register

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,7 +1,7 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
-
+const logDBMiddleware =require('../../middlewares/logDBMiddleware');
 
 // ************ Controller Require ************
 const {
@@ -12,6 +12,7 @@ const {
     forgetpw
 } = require('../controllers/mainController');
 
+
 // ************ Rutas Generales ************
 
 router.get('/', index); 
@@ -20,7 +21,7 @@ router.get('/search', search);
 
 router.get('/login', login); 
 
-router.get('/register', register); 
+router.get('/register', logDBMiddleware ,register); 
 
 router.get('/forgetpw', forgetpw); 
 
