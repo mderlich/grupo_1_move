c80c828823f83b64837c0945b8f7670584a63f61
unknown <alvarezrodrigofernando@gmail.com>
2022-08-10 21:09:13 -0300

    retoques en clase y dropdown

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,3 +1,26 @@
 /* 
 Aqui iran las funciones de JavaScripts
 */
+console.log("Hola desde el navegador")
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
